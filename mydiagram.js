885f0eee080782ccd7c5412179eb5abc7d53b7a0
Israel <israelhenrique@gmail.com>
2016-05-04 18:55:24 -0300

    add model

--- mydiagram.js
+++ mydiagram.js
@@ -1,5 +1,19 @@
-
 function init() {
-  var $ = go.GraphObject.make;
-  var myDiagram = $(go.Diagram, "teste");
-}
+
+var $ = go.GraphObject.make;
+var myDiagram =
+  $(go.Diagram, "teste",
+    {
+      initialContentAlignment: go.Spot.Center, // center Diagram contents
+      "undoManager.isEnabled": true // enable Ctrl-Z to undo and Ctrl-Y to redo
+    });
+
+var myModel = $(go.Model);
+// in our model data, each node is represented by a JavaScript object:
+myModel.nodeDataArray = [
+  { key: "Alpha" },
+  { key: "Beta" },
+  { key: "Gamma" }
+];
+myDiagram.model = myModel;
+  }
