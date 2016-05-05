05dbccec74fca522d9f377fdcba97e592753fd8e
Israel <israelhenrique@gmail.com>
2016-05-05 17:50:54 -0300

    improve extension inspector

--- mydiagram.js
+++ mydiagram.js
@@ -27,6 +27,7 @@ myDiagram =
           // some room around the text, a larger font, and a white stroke:
           { margin: 12, stroke: "white", font: "bold 16px sans-serif" },
           // TextBlock.text is data bound to the "name" attribute of the model data
+          new go.Binding("font", "font"),
           new go.Binding("text", "name"))
       );
 
@@ -34,10 +35,10 @@ myDiagram =
     model.nodeDataArray =
     [ // note that each node data object holds whatever properties it needs;
       // for this app we add the "name" and "source" properties
-      { key: "1",  background: "yellow", name: "Don Meow", color: "red" },
+      { key: "1",  background: "yellow", name: "Don Meow", color: "red", font: "bold 16px sans-serif"},
       { key: "2", parent: "1", name: "Copricat", color: "red"},
       { key: "3", parent: "1", name: "Demeter", color: "red" },
-      { key: "4", parent: "3", color: "red" }
+      { key: "4", parent: "3",  name: "Tsc", color: "red" }
     ];
 
 
@@ -46,19 +47,15 @@ myDiagram =
     var inspector = new Inspector('myInspectorDiv', myDiagram,
       {
         // uncomment this line to only inspect the named properties below instead of all properties on each object:
-        // includesOwnProperties: false,
+       includesOwnProperties: false,
         properties: {
           // key would be automatically added for nodes, but we want to declare it read-only also:
-          "key": { readOnly: true, show: Inspector.showIfPresent },
+          "name": { readOnly: true, show: Inspector.showIfPresent },
           // color would be automatically added for nodes, but we want to declare it a color also:
           "color": { show: Inspector.showIfPresent, type: 'color' },
-          // Comments and LinkComments are not in any node or link data (yet), so we add them here:
-          "Comments": { show: Inspector.showIfNode  },
-          "flag": { show: Inspector.showIfNode, type: 'boolean', defaultValue: true  },
-          "LinkComments": { show: Inspector.showIfLink },
+          "font": {  type: 'font'},
         }
       });
-
 }
 
 function treeLayout(){
